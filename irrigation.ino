18bcaa3cd43d3ce1fbd044fa6342648e7abc5322
Andrés Estévez <aecostas@gmail.com>
2016-12-14 21:57:13 +0100

    Renamed pump pin

--- irrigation.ino
+++ irrigation.ino
@@ -1,24 +1,28 @@
 const byte IRRIGATION = 0;
 const byte SLEEP = 1;
 
-const byte ledPin = 13;    // substitute by peristaltic bump pin
+const byte pumpPin = 13;    // substitute by peristaltic bump pin
 const byte buttonPin = 2;
 const int irrigationTime = 1000;
 
 unsigned long lastIrrigation = 0;
+unsigned long buttonFalling = 0;
+
 int timeout = 5000;
 byte state = SLEEP;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+  pinMode(pumpPin, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
-  digitalWrite(ledPin, LOW);
-  attachInterrupt(digitalPinToInterrupt(buttonPin), handler, RISING);
+  digitalWrite(pumpPin, LOW);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), handler_rising, RISING);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), handler_falling, FALLING);
 }
 
 /**
- * Set irrigation pin to HIGH during the specific duration
+ * Set irrigation pin to HIGH during the specific duration.
+ * (blocking function)
  * 
  * @param pin - irrigation pin
  * @param duration - duration of the irrigation (millis)
@@ -34,30 +38,36 @@ void irrigate(byte pin,int duration) {
    Serial.print("End irrigation\n");
    lastIrrigation = millis();
    state = SLEEP;
+}
 
+void handler_falling() {
+    buttonFalling = millis();
 }
 
 
-void handler() {
+void handler_rising() {
   if (IRRIGATION == state) {
-      return;
+    return;
   } else {
-      state = IRRIGATION; 
+    state = IRRIGATION; 
   }
 }
 
 void loop() {
+  // TODO: hacer diagrama de estado incluyendo el estado
+  // pulsado el boton de regado
+
 
   // irrigate after pressing the button
   // or on timeout from last irrigation
   
-     if (IRRIGATION == state) {
-         irrigate(ledPin, irrigationTime); 
-     }  
+  if (IRRIGATION == state) {
+    irrigate(pumpPin, irrigationTime); 
+  }
 
-     if (millis() - lastIrrigation >= timeout) {
-         irrigate(ledPin, irrigationTime);
-     }
+  if (millis() - lastIrrigation >= timeout) {
+    irrigate(pumpPin, irrigationTime);
+  }
 
 }
 
